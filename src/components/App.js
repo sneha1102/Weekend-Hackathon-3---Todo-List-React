b89e8a6608978131a07b16fa30f8ab8acaacf0f6
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-11-07 11:25:40 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,100 @@
+import { useState } from "react";
 import React from "react";
-import "./../styles/App.css";
-import ToDo from "./toDo";
-import { Button } from "@material-ui/core";
 
-function App() {
-  const tasks = [];
-  const [task, setTask] = React.useState(tasks);
-  const [currTask, setCurrTask] = React.useState("");
+export default function App() {
+  const [inputBox, setInputBox] = useState("");
+  const [editBoxText, setEditBoxText] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  const [taskId, setTaskId] = useState(0);
+  const [showEditBox, setShowEditBox] = useState(false);
+  const [editId, setEditId] = useState(1);
 
-  const handleDelete = (index) => {
-    const arrCopy = [...task];
-    arrCopy.splice(index, 1);
-    //console.log(arrCopy);
-    setTask(arrCopy);
+  const handleInputBoxChange = (event) => {
+    let text = event.target.value;
+    setInputBox(text);
   };
-  const handleSubmit = (currTask) => {
-    if (currTask !== "") {
-      const arrCopy = [...task];
-      arrCopy.push(currTask);
 
-      setTask(arrCopy);
-    }
+  const handleAddTask = () => {
+    let tempList = [...toDoList];
+    tempList.push({ id: taskId, text: inputBox });
+    setTaskId(taskId + 1);
+    setToDoList(tempList);
+    setInputBox("");
   };
-  const handleSave = (currTask, index) => {
-    let arrCopy = [...task];
-    arrCopy.splice(index, 1, currTask);
-    setTask(arrCopy);
+
+  const handleDelete = (id) => {
+    let newToDoList = toDoList.filter((task) => task !== toDoList[id]);
+    setToDoList(newToDoList);
   };
+
+  const handleEdit = (id) => {
+    setShowEditBox(!showEditBox);
+    setEditId(id);
+  };
+
+  const handleEditBox = (event) => {
+    let text = event.target.value;
+    setEditBoxText(text);
+  };
+
+  const saveEdit = () => {
+    const tempList = [...toDoList];
+    tempList.forEach((task) => {
+      if (task.id === editId) {
+        task.text = editBoxText;
+      }
+    });
+    setToDoList(tempList);
+    setShowEditBox(false);
+    setEditBoxText("");
+  };
+
   return (
-    <div id="main">
-      <textarea
-        id="task"
-        // variant="outlined"
-        placeholder="add task...."
-        value={currTask}
-        onChange={(event) => setCurrTask(event.target.value)}
-      />
-      <Button
-        id="btn"
-        variant="contained"
-        color="secondary"
-        onClick={(event) => {
-          event.preventDefault();
-          if (currTask !== "") {
-            handleSubmit(currTask);
-            setCurrTask("");
-          }
-        }}
-      >
-        Add
-      </Button>
-      <ToDo tasks={task} onDelete={handleDelete} onSave={handleSave} />
-    </div>
+    <>
+      <div id="main">
+        <textarea id="task" value={inputBox} onChange={handleInputBoxChange} />
+        <button id="btn" disabled={!inputBox} onClick={handleAddTask}>
+          Add Task
+        </button>
+        <hr />
+        {toDoList.length === 0 ? (
+          <div>No Tasks</div>
+        ) : (
+          <ul>
+            {toDoList.map((task, id) => {
+              return (
+                <>
+                  <li key={id} className="list">
+                    {task.text}
+                  </li>
+                  <button className="edit" onClick={() => handleEdit(id)}>
+                    Edit
+                  </button>
+                  <button className="delete" onClick={() => handleDelete(id)}>
+                    Delete
+                  </button>
+                </>
+              );
+            })}
+          </ul>
+        )}
+        {showEditBox ? (
+          <div>
+            <textarea
+              className="editTask"
+              value={editBoxText}
+              onChange={handleEditBox}
+            />
+            <button
+              className="saveTask"
+              disabled={!editBoxText}
+              onClick={() => saveEdit()}
+            >
+              Save
+            </button>
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 }
-
-export default App;
